61acf06688454cfa79f97a4451673f698ae39a73
SnehaSriwastava <sriwastavasneha@gmail.com>
2026-02-10 12:38:32 +0530

    / route

--- server.js
+++ server.js
@@ -28,6 +28,12 @@ function errorResponse(msg){
     }
 }
 
+app.get("/",(req,res)=>{
+   res.json({
+      message:"Bajaj BFHL API is running"
+   });
+});
+
 app.get("/health",(req,res)=>{
     res.status(200).json({
         is_success:true,
